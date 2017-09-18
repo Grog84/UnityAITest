80894519cf223fa25a14176ba09176b5c1175ba0
Fabio Brigidi <fabio.brigidi@gmal.com>
2017-09-18 09:00:20 +0200

    First Commit

---
+++ Assets/Scripts/AgentBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBehaviour : MonoBehaviour {
+
+    public GameObject target;
+    protected Agent agent;
+
+    public virtual Steering GetSteering()
+    {
+        return new Steering();
+    }
+
+    public virtual void Awake()
+    {
+        agent = gameObject.GetComponent<Agent>();
+    }
+
+    public virtual void Update()
+    {
+        agent.SetSteering(GetSteering());
+    }
+
+}
